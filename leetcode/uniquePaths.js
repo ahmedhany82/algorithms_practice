d47efe0cd6dc90e1f04e731c90785a698ffcf5be
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-03-20 16:42:06 +0100

    added 62. Unique Paths recursive approach

---
+++ leetcode/uniquePaths.js
@@ -0,0 +1,16 @@
+/*  Recursive approach  
+    Time complexity: O(2^(m+n))
+    Space complexity: O(m+n) 
+    Times out on m=51,n=9
+*/
+
+const uniquePaths = function(m,n) {
+    if( m === 1 && n === 1) return 1;
+    if( m === 0 || n === 0) return 0;
+
+    return uniquePaths(m-1, n) + uniquePaths(m, n-1);
+}
+
+
+console.log(uniquePaths(23,12))  //193536720  Runtime: 9408ms
+console.log(uniquePaths(51,9))  //1916797311  Times out

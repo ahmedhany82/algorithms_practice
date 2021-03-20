853154ce57ba54da9295ec37bfb33148edfcde81
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-03-20 16:46:55 +0100

    62. Unique Paths - memoization

--- leetcode/uniquePaths.js
+++ leetcode/uniquePaths.js
@@ -4,13 +4,29 @@
     Times out on m=51,n=9
 */
 
-const uniquePaths = function(m,n) {
+// const uniquePaths = function(m,n) {
+//     if( m === 1 && n === 1) return 1;
+//     if( m === 0 || n === 0) return 0;
+
+//     return uniquePaths(m-1, n) + uniquePaths(m, n-1);
+// }
+
+// console.log(uniquePaths(23,12))  //193536720  Runtime: 9408ms
+// console.log(uniquePaths(51,9))  //1916797311  Times out
+
+
+/* Using memoization */
+
+const uniquePaths = function(m, n, memo={}) {
+    const key = m + ',' + n;
+    if(key in memo) return memo[key];
     if( m === 1 && n === 1) return 1;
     if( m === 0 || n === 0) return 0;
 
-    return uniquePaths(m-1, n) + uniquePaths(m, n-1);
+    memo[key] =  uniquePaths(m-1, n, memo) + uniquePaths(m, n-1, memo);
+    return memo[key];
 }
 
+console.log(uniquePaths(23,12))  //193536720  Runtime: 68ms
+console.log(uniquePaths(51,9))  //1916797311  Runtime: 104ms
 
-console.log(uniquePaths(23,12))  //193536720  Runtime: 9408ms
-console.log(uniquePaths(51,9))  //1916797311  Times out

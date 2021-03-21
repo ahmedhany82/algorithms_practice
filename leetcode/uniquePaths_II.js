57f0e903e5a068a684a02740030a2583ba2cbddf
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-03-21 23:43:04 +0100

    63. Unique Paths II optimized version

--- leetcode/uniquePaths_II.js
+++ leetcode/uniquePaths_II.js
@@ -3,24 +3,24 @@
 // Space complexity: O(m*n)
 
 const uniquePathsWithObstacles = (obstacleGrid) => {
-    const m = obstacleGrid.length; //rows
-    const n = obstacleGrid[0].length; //cols
-    const table = Array(m+1).fill().map(() => Array(n+1).fill(0));
-    table[1][1] = 1;
-    for(let i=0; i<m; i++) {
-        for(let j=0; j<n; j++) {
-            if(obstacleGrid[i][j] === 1) table[i+1][j+1] = null;
-        }
-    }
 
-    for(let i=0; i<=m; i++) {
-        for(let j=0; j<=n; j++) {
-            const current = table[i][j];
-            if(table[i][j] !== null) {
-                if(i+1 <= m && table[i+1][j] !== null) table[i+1][j] += current;
-                if(j+1 <= n && table[i][j+1] !== null) table[i][j+1] += current;    
+    if(obstacleGrid[0][0] === 1) return 0;
+    
+    obstacleGrid[0][0] = 1;
+        
+    const rows = obstacleGrid.length;
+    const cols = obstacleGrid[0].length;
+    
+    for(let i= 0; i<rows; i++) {
+        for(let j = 0; j < cols; j++) {
+            if( !(i === 0 && j === 0) ) {
+                if(obstacleGrid[i][j] !== 1) {
+                    obstacleGrid[i][j] = (i-1 >= 0 ? obstacleGrid[i-1][j] : 0) + (j-1 >=0 ? obstacleGrid[i][j-1] : 0);   
+                } else {
+                    obstacleGrid[i][j] = 0;                
+                }                
             }
         }
     }
-    return table[m][n];
+    return obstacleGrid[rows-1][cols-1];
 };

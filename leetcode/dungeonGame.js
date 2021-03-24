b36818cc3aacb1a028c2c4e44f5e53f81d3d550b
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-03-24 22:54:05 +0100

    added skeleton for Dungeon Game

---
+++ leetcode/dungeonGame.js
@@ -0,0 +1,8 @@
+// 174. Dungeon Game - Hard
+
+const calculateMinimumHP = dungeon => {
+
+};
+
+console.log(calculateMinimumHP([[-2,-3,3],[-5,-10,1],[10,30,-5]])); // 7
+console.log(calculateMinimumHP([[0]])); // 1

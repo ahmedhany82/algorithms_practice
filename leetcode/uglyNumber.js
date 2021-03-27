e85f1049444258cda1d5f747df62aacf7d1435cf
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-03-27 20:38:16 +0100

    added 263. Ugly Number

---
+++ leetcode/uglyNumber.js
@@ -0,0 +1,25 @@
+const isUgly = (n, memo={}) => {
+    if(n in memo) return memo[n];
+    if(n === 0) return false;
+    if(n === 1) return true;
+    if(!Number.isInteger(n)) return false;
+
+    const factors = [2, 3, 5];
+
+    for(factor of factors) {
+        let newNumber = n / factor;
+        let result = isUgly(newNumber);
+        if(result === true) {
+            memo[n] = true;
+            return memo[n];
+        }
+    }
+    memo[n] = false;
+    return memo[n];
+};
+
+
+console.log(isUgly(6));  //true
+console.log(isUgly(8));  //true
+console.log(isUgly(14)); //false
+console.log(isUgly(1));  //true

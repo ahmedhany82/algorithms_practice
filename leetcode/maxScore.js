20e94951f4c3f9c25d95c24caadb4992bdadecb4
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-04-02 23:53:24 +0200

    added test examples to maxScore

--- leetcode/maxScore.js
+++ leetcode/maxScore.js
@@ -3,3 +3,9 @@
 const maxScore = function(cardPoints, k) {
     
 };
+
+
+
+const testarr1 = [9,4,2,10,7,8,8,1,9]; //Answer: 5
+const testarr2 = [4,8,12,16]; //Answer: 2
+const testarr3 = [100]; //Answer: 1

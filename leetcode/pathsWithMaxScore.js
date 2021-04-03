1a691fcd0d278e5e098465e678d0898fc77b79c6
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-04-03 23:40:49 +0200

    started 1301. Number of Paths with Max Score

---
+++ leetcode/pathsWithMaxScore.js
@@ -0,0 +1,10 @@
+//1301. Number of Paths with Max Score - Hard
+
+const pathsWithMaxScore = function(board) {
+    
+};
+
+
+console.log(pathsWithMaxScore( ["E23","2X2","12S"] )); //Answer: [7,1]
+console.log(pathsWithMaxScore( ["E12","1X1","21S"] )); //Answer: [4,2]
+console.log(pathsWithMaxScore( ["E11","XXX","11S"] )); //Answer: [0,0]

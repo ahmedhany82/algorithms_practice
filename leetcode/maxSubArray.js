29dc8429e8ff5442568124e049baafd59deb96f6
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-04-05 23:36:57 +0200

    added 53. Maximum Subarray

---
+++ leetcode/maxSubArray.js
@@ -0,0 +1,14 @@
+// 53. Maximum Subarray - Easy
+
+const maxSubArray = function(nums) {
+
+    const grid = Array(nums.length).fill().map(() => Array(nums.length).fill(0));
+
+    
+    
+};
+
+
+console.log(maxSubArray( [5,4,-1,7,8] )); //Answer: 23
+console.log(maxSubArray( [-2,1,-3,4,-1,2,1,-5,4] )); //Answer: 6
+console.log(maxSubArray( [1] )); //Answer: 1

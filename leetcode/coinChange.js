ec50a90f06574877221527ae8ed2ee8073e80c9e
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-04-06 23:25:31 +0200

    first attempt coinChange

--- leetcode/coinChange.js
+++ leetcode/coinChange.js
@@ -1,5 +1,22 @@
 const coinChange = function(coins, amount) {
     
+    const table = Array(amount + 1).fill(null);
+    table[0] = [];
+
+    for(let i=0; i<table.length; i++) {
+        if(table[i] !== null) {
+            for(element of coins) {
+                if(i + element < table.length)
+                {
+                    const combination = [ ...table[i], element];
+                    if(!table[i + element] || table[i + element].length > combination.length) {
+                        table[i + element] = combination;
+                    }    
+                }
+            }
+        }
+    }
+    return table[amount] ? table[amount].length : -1;
 };
 
 console.log(coinChange([1,2,5],11)); //Answer: 3

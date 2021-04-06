556fe285b8f9b1f2d48fe2562237b259ecbc308a
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-04-06 23:00:29 +0200

    added coinChange.js

---
+++ leetcode/coinChange.js
@@ -0,0 +1,9 @@
+const coinChange = function(coins, amount) {
+    
+};
+
+console.log(coinChange([1,2,5],11)); //Answer: 3
+console.log(coinChange([2],3));      //Answer:-1
+console.log(coinChange([1],0));      //Answer: 0
+console.log(coinChange([1],1));      //Answer: 1
+console.log(coinChange([1],2));      //Answer: 2

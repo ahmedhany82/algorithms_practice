a78c2883f1cbc08428f57823be69833570b2f7db
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-04-07 22:49:36 +0200

    152. Maximum Product Subarray

---
+++ leetcode/maxProduct.js
@@ -0,0 +1,8 @@
+// 152. Maximum Product Subarray - Medium
+
+const maxProduct = function(nums) {
+    
+};
+
+console.log(maxProduct([2,3,-2,4])); //Answer: 6
+console.log(maxProduct([-2,0,-1])); //Answer: 0

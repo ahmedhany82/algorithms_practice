c0f235a79072a836196d8aa87e15f7532453abe1
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-04-08 23:52:11 +0200

    added 628. Maximum Product of Three Numbers

---
+++ leetcode/maximumProductThreeNums.js
@@ -0,0 +1,9 @@
+/* 628. Maximum Product of Three Numbers */
+
+var maximumProduct = function(nums) {
+    
+};
+
+console.log(maximumProduct([1,2,3])); // 6
+console.log(maximumProduct([1,2,3,4])); // 24
+console.log(maximumProduct([-1,-2,-3])); // -6

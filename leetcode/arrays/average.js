193925cc2628e937fa11f4b799b66adcbe3cb6b3
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-05-03 23:32:16 +0200

    added 1491. Average Salary

---
+++ leetcode/arrays/average.js
@@ -0,0 +1,19 @@
+// 1491. Average Salary Excluding the Minimum and Maximum Salary
+
+var average = function(salary) {
+    
+    const numEmployees = salary.length;
+    let max = salary[0];
+    let min = salary[0];
+    let average = 0;
+
+    for(let i = 0; i < numEmployees; i++) {
+            if(salary[i] > max) max = salary[i]
+            if(salary[i] < min) min = salary[i]
+            average += salary[i]
+    }
+
+    average = (average - max - min) / (numEmployees - 2);
+    return average;
+    
+};

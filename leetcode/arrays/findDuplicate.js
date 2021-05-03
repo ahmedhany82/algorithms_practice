0b600380a894482e7538d3a54f6fb5d461d655a5
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-05-03 23:30:05 +0200

    added 287.findDuplicate

---
+++ leetcode/arrays/findDuplicate.js
@@ -0,0 +1,18 @@
+// 287. Find the Duplicate Number - Medium
+
+var findDuplicate = function(nums) {
+    
+    let map = {};
+    for(let i=0; i < nums.length; i++) {
+        if(!(nums[i] in map)) {
+            map[nums[i]] = 1;
+        } else {
+            return nums[i];
+        }
+    } 
+};
+
+console.log([1,3,4,2,2]); //Answer: 2
+console.log([3,1,3,4,2]); //Answer: 3
+console.log([1,1]); //Answer: 1
+console.log([1,1,2]); //Answer: 1 

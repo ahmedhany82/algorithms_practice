dd3ad5773b51e32504e1fc34a8a16a1a8101a6fe
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-05-08 13:26:02 +0200

    added canConstruct recursive

--- practice/canConstruct.js
+++ practice/canConstruct.js
@@ -1,15 +1,27 @@
 const canConstruct = (target, wordBank) => {
 
+    if(target === '') return true;
 
+    for(let word of wordBank) {
+        if(target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            if(canConstruct(suffix, wordBank) === true) {
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
 
 console.log(canConstruct("abcdef", ["ab","abc","cd","def", "abcd"])); // true
 console.log(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); // false
-console.log(canConstruct("enterapotenpot", ["a", "p", "ent", "enter", "ot", "o", "t"])); // true
-console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeef", [
+console.log(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])); // true
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
     "e",
     "ee",
     "eee",
+    "eeee",
     "eeeee",
     "eeeeee"
 ])); // false
